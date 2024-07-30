3efcad0c43a7cebb53deb8a2ac9ef46964c832f9
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-07-31 00:07:36 +0300

    Hotfix

---
+++ DockerDB/init.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS aliOrders;
+
+USE aliOrders;
+
+CREATE TABLE clients
+(
+    id_client  binary(16) primary key not null unique,
+    login_name varchar(45)            not null,
+    password   varchar(225)           not null
+);
+
+CREATE TABLE orders
+(
+    id_order     INT primary key not null,
+    email        varchar(225)    not null,
+    order_number varchar(225)    not null,
+    description  varchar(225)    not null,
+    id_client    binary(16)
+);
+
+ALTER TABLE orders
+    ADD CONSTRAINT `fk_clents_orders` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id_client`);
