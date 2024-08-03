ebe8026cc79651c09327ab71de4750a5d161a077
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-08-04 00:53:38 +0300

    Hotfix

--- src/main/java/com/example/securityproject/entities/Order.java
+++ src/main/java/com/example/securityproject/entities/Order.java
@@ -4,15 +4,17 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.nio.ByteBuffer;
+import java.util.UUID;
+
 @Getter
 @Setter
 @Entity
 @Table(name = "orders")
 public class Order {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_order")
-    private Long id_order;
+    private byte[] id_order;
     @Column(name = "email")
     private String email;
     @Column(name = "order_number")
@@ -21,6 +23,12 @@ public class Order {
     private String description;
     @Column(name = "id_client")
     private byte[] clientId;
+    public void setIdClientUUID(UUID uuid) {
+        ByteBuffer byteBuffer = ByteBuffer.wrap(new byte[16]);
+        byteBuffer.putLong(uuid.getMostSignificantBits());
+        byteBuffer.putLong(uuid.getLeastSignificantBits());
+        this.id_order = byteBuffer.array();
+    }
     @Override
     public String toString() {
         return email +
