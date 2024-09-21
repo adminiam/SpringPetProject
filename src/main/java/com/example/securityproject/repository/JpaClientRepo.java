d1acd3c3cdf010e6c336ce632741774e10ccaa09
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-09-22 02:17:39 +0300

    Security improvement

--- src/main/java/com/example/securityproject/repository/JpaClientRepo.java
+++ src/main/java/com/example/securityproject/repository/JpaClientRepo.java
@@ -1,17 +1,13 @@
 package com.example.securityproject.repository;
 
-
 import com.example.securityproject.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface JpaClientRepo extends JpaRepository<Client,byte[]> {
-    @Query("select c.loginName from Client c")
 
     @Query("select c.password from Client c where c.loginName = :username")
     String getClientPasswordByLoginName(@Param("username") String username);
