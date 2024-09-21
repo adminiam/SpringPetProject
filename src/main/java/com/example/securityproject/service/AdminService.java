df04fec8f15a689c5a42eab7e95d3b8e1110d637
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-09-22 02:16:07 +0300

    Security improvement

--- src/main/java/com/example/securityproject/service/AdminService.java
+++ src/main/java/com/example/securityproject/service/AdminService.java
@@ -1,11 +1,15 @@
 package com.example.securityproject.service;
 
 
+import com.example.securityproject.entities.Client;
 import com.example.securityproject.exception.SuppressedStackTraceException;
 import com.example.securityproject.repository.JpaClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.servlet.view.RedirectView;
+
 
 @Service
 public class AdminService {
@@ -18,5 +22,20 @@ public class AdminService {
         } catch (Exception e) {
             throw new SuppressedStackTraceException("Error occurred " + e.getMessage());
         }
+    }
+    public RedirectView deleteOrder(String userName) {
+        try {
+            Client client = jpaClientRepo.getClientByLoginName(userName);
+            if (client != null) {
+                jpaClientRepo.delete(client);
+                return new RedirectView("/adminPanel", true);
+            }
+
+        } catch (DataAccessException e) {
+            throw new SuppressedStackTraceException("Error occurred " + e.getMessage());
+        } catch (Exception e) {
+            throw new SuppressedStackTraceException("Error occurred " + e.getMessage());
+        }
+        return new RedirectView("/error", true);
     }
 }
