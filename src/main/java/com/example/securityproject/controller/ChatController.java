26bc37de09ff5eb50657ad68a5666b88b6aac187
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-10-09 00:20:09 +0300

    Kafka improvement

--- src/main/java/com/example/securityproject/controller/ChatController.java
+++ src/main/java/com/example/securityproject/controller/ChatController.java
@@ -4,12 +4,15 @@ import com.example.securityproject.service.ChatProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class ChatController {
     @Autowired
-    ChatProducer chatProducer;
+    private ChatProducer chatProducer;
+
     @GetMapping("/send")
-    public String getMessage(){
+    public String sendMessage(){
         return chatProducer.sendMessage();
     }
+
 }
--- src/main/java/com/example/securityproject/service/ChatConsumer.java
+++ src/main/java/com/example/securityproject/service/ChatConsumer.java
@@ -2,17 +2,24 @@ package com.example.securityproject.service;
 
 import com.example.securityproject.models.Message;
 
+import lombok.Getter;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class ChatConsumer {
     private final Gson gson = new Gson();
+    @Getter
+    List<Message> list = new ArrayList<>();
 
     @KafkaListener(topics = "chat_topic", groupId = "myGroup")
     public void listen(String message) {
         Message parsedMessage = gson.fromJson(message, Message.class);
+        list.add(parsedMessage);
         System.out.println(parsedMessage.getMessage());
         System.out.println(parsedMessage.getId());
     }
