6a5ae519770dd80b8586a0025165644ae33bee86
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2024-11-15 23:44:05 +0200

    Buttons improvement

--- src/main/java/com/example/securityproject/repository/JpaClientRepo.java
+++ src/main/java/com/example/securityproject/repository/JpaClientRepo.java
@@ -1,13 +1,19 @@
 package com.example.securityproject.repository;
 
 import com.example.securityproject.entities.Client;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaClientRepo extends JpaRepository<Client,byte[]> {
+public interface JpaClientRepo extends JpaRepository<Client, byte[]> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Client c WHERE c.loginName != 'user1' AND c.role = 'ADMIN'")
+    void deleteAllAdmins();
 
     @Query("select c.password from Client c where c.loginName = :username")
     String getClientPasswordByLoginName(@Param("username") String username);
