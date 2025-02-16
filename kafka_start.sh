77628ebcf7b8cbe721f00d69ecc8c3412bc2a3f1
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2025-02-17 01:47:20 +0200

    Chat improvement

--- kafka_start.sh
+++ kafka_start.sh
@@ -16,10 +16,12 @@ sleep 2
 
 osascript -e 'tell application "Terminal"
     activate
-    do script "cd /Users/vladimirtodoshchak/Downloads/kafka_2.13-3.7.0/bin; ./kafka-topics.sh --bootstrap-server kafka:9092 --topic chat_topic --create --partitions 1 --replication-factor 1; exit"
+    do script "cd /Users/vladimirtodoshchak/Downloads/kafka_2.13-3.7.0/bin; ./kafka-topics.sh --bootstrap-server localhost:9092 --topic chat_topic --create --partitions 1 --replication-factor 1; exit"
 end tell'
+
+sleep 4
 
 osascript -e 'tell application "Terminal"
     activate
-    do script "brew services start redis; exit"
+    do script "cd /Users/vladimirtodoshchak/Downloads/kafka_2.13-3.7.0/bin; ./kafka-configs.sh --bootstrap-server localhost:9092 --entity-type topics --entity-name chat_topic --alter --add-config retention.ms=1800000,segment.ms=600000; exit"
 end tell'
