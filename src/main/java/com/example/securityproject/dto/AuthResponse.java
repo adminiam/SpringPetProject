95b0388c9214bee2c7297462cfd3b52e3ad9905b
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2025-03-12 23:47:37 +0200

    General improvement

--- src/main/java/com/example/securityproject/dto/AuthResponse.java
+++ src/main/java/com/example/securityproject/dto/AuthResponse.java
@@ -1,19 +1,16 @@
 package com.example.securityproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
-
     private String role;
     private String message;
     private String token;
-
-    public AuthResponse(String role, String message, String token) {
-        this.role = role;
-        this.message = message;
-        this.token = token;
-    }
 }
