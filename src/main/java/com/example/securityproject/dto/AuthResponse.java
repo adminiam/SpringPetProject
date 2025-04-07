61601c68a5df86c298f53fe99b41bc6a504d2950
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2025-04-07 23:29:48 +0300

    Tests

--- src/main/java/com/example/securityproject/dto/AuthResponse.java
+++ src/main/java/com/example/securityproject/dto/AuthResponse.java
@@ -1,10 +1,14 @@
 package com.example.securityproject.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AuthResponse {
     private String role;
     private String message;
--- src/test/java/com/example/securityproject/controllerTest/AdminControllerTest.java
+++ src/test/java/com/example/securityproject/controllerTest/AdminControllerTest.java
@@ -61,13 +61,66 @@ public class AdminControllerTest {
         Response trackingResponse = given()
                 .header("Authorization", "Bearer " + token)
                 .cookies(authResponse.getCookies())
-                .queryParam("userName", "user3")
-                .queryParam("password", "Zxcvb122")
-                .queryParam("role", "USER")
+                .contentType("application/json")
+                .body("{\"userName\": \"user3\", \"password\": \"Zxcvb122\", \"role\": \"USER\"}")
                 .when()
                 .log().all()
                 .post("/adminPanel/createClient");
+
         trackingResponse.then().log().body();
+
+        assertEquals(200, trackingResponse.getStatusCode(), "Не удалось получить трекинг-номера");
+    }
+    @Test
+    public void testAuthenticationAndDeleteUsersAdmin() {
+        Response authResponse = given()
+                .contentType("application/json")
+                .body("{\"username\": \"user1\", \"password\": \"12345678\"}")
+                .when()
+                .post("/api/auth/login");
+
+        assertEquals(200, authResponse.getStatusCode(), "Аутентификация не удалась");
+
+        String token = authResponse.jsonPath().getString("token");
+        assertNotNull(token, "Токен не получен");
+
+        Response trackingResponse = given()
+                .header("Authorization", "Bearer " + token)
+                .cookies(authResponse.getCookies())
+                .contentType("application/json")
+                .body("{\"userName\": \"user5\"}")
+                .when()
+                .log().all()
+                .post("/adminPanel/deleteClient");
+
+        trackingResponse.then().log().body();
+
+        assertEquals(200, trackingResponse.getStatusCode(), "Не удалось получить трекинг-номера");
+    }
+    @Test
+    public void testAuthenticationAndDeleteAllAdmin() {
+        Response authResponse = given()
+                .contentType("application/json")
+                .body("{\"username\": \"user1\", \"password\": \"12345678\"}")
+                .when()
+                .post("/api/auth/login");
+
+        assertEquals(200, authResponse.getStatusCode(), "Аутентификация не удалась");
+
+        String token = authResponse.jsonPath().getString("token");
+        assertNotNull(token, "Токен не получен");
+
+        Response trackingResponse = given()
+                .header("Authorization", "Bearer " + token)
+                .cookies(authResponse.getCookies())
+                .contentType("application/json")
+                .body("{\"userName\": \"user5\"}")
+                .when()
+                .log().all()
+                .post("/adminPanel/deleteClient");
+
+        trackingResponse.then().log().body();
+
         assertEquals(200, trackingResponse.getStatusCode(), "Не удалось получить трекинг-номера");
     }
 }
