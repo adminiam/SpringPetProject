e1dae011c982a91b58887231e8810b931ecc4a66
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2025-04-19 00:28:20 +0300

    Authentication fix

--- src/main/java/com/example/securityproject/controller/AuthController.java
+++ src/main/java/com/example/securityproject/controller/AuthController.java
@@ -1,8 +1,11 @@
 package com.example.securityproject.controller;
 
 import com.example.securityproject.dto.LoginRequest;
+import com.example.securityproject.dto.RegisterRequest;
 import com.example.securityproject.service.AuthenticationService;
+import com.example.securityproject.service.RegistryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +14,13 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     @Autowired
     AuthenticationService authenticationService;
+    @Autowired
+    RegistryService registryService;
+
+    @PostMapping("register")
+    public HttpStatus register(@RequestBody RegisterRequest registerRequest) {
+        return registryService.register(registerRequest.getName(), registerRequest.getPassword());
+    }
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
