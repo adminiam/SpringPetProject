e14b89a0e82636c1fd8a4c953c8a8a906a7b9022
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2025-04-19 01:02:06 +0300

    Authentication fix

--- src/main/java/com/example/securityproject/service/AuthenticationService.java
+++ src/main/java/com/example/securityproject/service/AuthenticationService.java
@@ -2,11 +2,12 @@ package com.example.securityproject.service;
 
 import com.example.securityproject.components.JwtTokenProvider;
 import com.example.securityproject.components.UserContext;
-import com.example.securityproject.dto.AuthResponse;
 import com.example.securityproject.dto.LoginRequest;
 import com.example.securityproject.entities.Client;
 import com.example.securityproject.repository.JpaClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -46,8 +47,16 @@ public class AuthenticationService {
             userContext.setRole(clientData.getRole());
 
             String token = jwtTokenProvider.generateToken(authentication);
-            return ResponseEntity.ok(new AuthResponse(clientData.getRole(), "Добро пожаловать, " + request.getUsername(), token));
 
+            ResponseCookie cookie = ResponseCookie.from("token", token)
+                    .httpOnly(true)
+                    .secure(true)
+                    .path("/")
+                    .sameSite("Strict")
+                    .maxAge(60 * 60)
+                    .build();
+
+            return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString()).build();
         } catch (Exception e) {
             return ResponseEntity.status(403).body("Authentication failed");
         }
