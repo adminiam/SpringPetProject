47e3dabbfc433fcf6da881ccf5e3e04152ab6902
Vladimir Todoshchak <121573107+adminiam@users.noreply.github.com>
2025-05-20 00:04:28 +0300

    Refresh token added

--- src/main/java/com/example/securityproject/components/JwtTokenProvider.java
+++ src/main/java/com/example/securityproject/components/JwtTokenProvider.java
@@ -11,16 +11,35 @@ import java.util.Date;
 
 @Component
 public class JwtTokenProvider {
+
+
     private final SecretKey key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-    public String generateToken(Authentication authentication) {
+    public String generateAccessToken(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Date now = new Date();
+        long accessTokenValidity = 15 * 60 * 1000;
+        Date expiryDate = new Date(now.getTime() + accessTokenValidity);
 
-        long jwtExpirationMs = 86400000;
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
-                .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .claim("role", userDetails.getAuthorities().stream().findFirst().get().getAuthority())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key)
+                .compact();
+    }
+
+    public String generateRefreshToken(Authentication authentication) {
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        Date now = new Date();
+        long refreshTokenValidity = 7 * 24 * 60 * 60 * 1000;
+        Date expiryDate = new Date(now.getTime() + refreshTokenValidity);
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
                 .signWith(key)
                 .compact();
     }
@@ -33,6 +52,7 @@ public class JwtTokenProvider {
                 .getBody()
                 .getSubject();
     }
+
 
     public boolean validateToken(String token) {
         try {
